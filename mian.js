19793da8d158872d870767835afb4aabf0356e3d
ManuelSanchezRomero <manuel.sromero@outlook.com>
2022-12-21 21:15:25 -0300

    agregando array y objetos

--- mian.js
+++ mian.js
@@ -1,6 +1,29 @@
 let dias = 0
 let precio = 0
 
+// Lista de productos
+function Lugares(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio
+}
+const gtav = new Lugares("GTA 5", 4000);
+const mario = new Lugares("Mario", 4200);
+const pokemon = new Lugares("Pokemon", 4300);
+const zelda = new Lugares("Zelda", 4500);
+const got = new Lugares("GoT", 5000);
+const hp = new Lugares("Harry Potter", 9000);
+const anillos = new Lugares("Señor de los Anillos", 6000);
+const wakanda = new Lugares("Wakanda", 6500);
+const bosque = new Lugares("Bosque Encantado", 8000);
+const univ = new Lugares("Universo", 7500);
+const futuro = new Lugares("Futuro", 8200);
+const hosp = new Lugares("Hospital Abandonado", 9500);
+
+const stock = [gtav, mario, pokemon, zelda, got, hp, anillos, wakanda, bosque, univ, futuro, hosp]
+
+
+
+//Funcion del boton
 function consultaDias(precio) {
     let dias = parseInt(prompt("¿Cuantos dias quiere contratar?: "));
     //En caso de que ponga 0 dias
@@ -14,21 +37,17 @@ function consultaDias(precio) {
     if (vip === 1) {
         suma += suma * 0.2;
     }
+    let resultado = dias + " dias" + " x" + " $" + precio + "=" + " $" + suma
+    let carrito = []
+    carrito.push(resultado)
 
     //Imprimir en pantalla y en consola
-    document.getElementById("carro").innerHTML = dias + " dias" + " x " + " $" + precio + "=" + " $" + suma
-    console.log(dias + " dia/s" + " x " + " $" + precio + "=" + " $" + suma);
+    console.log(carrito);
+    document.getElementById("carro").innerHTML = carrito
 }
 
 
-
-
-
-
-
-
-
-
+//-----------------------------------------------BORRADOR ----------------------------------------------------------------------
 
 
 // if (dias > 0) {
@@ -43,3 +62,27 @@ function consultaDias(precio) {
 
 // for (let contador = 1; contador <= cantCoti; contador++) {
 //     let dias = parseInt(prompt("Cuantos dias quieres contratar?"));
+
+
+
+
+
+// stock = [gtav, mario, pokemon, zelda, got, hp, anillos, wakanda, bosque, univ, futuro, hosp];
+// console.log(stock);
+
+// function lugares(nombre, precio, ) {
+//     this.nombre = nombre;
+//     this.precio = precio
+// }
+// const gtav = new lugares(gtav, 4000);
+// const mario = new lugares(mario, 4200);
+// const pokemon = new lugares(pokemon, 4300);
+// const zelda = new lugares(zelda, 4500);
+// const got = new lugares(got, 5000);
+// const hp = new lugares(hp, 9000);
+// const anillos = new lugares(anillos, 6000);
+// const wakanda = new lugares(wakanda, 6500);
+// const bosque = new lugares(bosque, 8000);
+// const univ = new lugares(universo, 7500);
+// const futuro = new lugares(futuro, 8200);
+// const hosp = new lugares(hospital, 9500);
